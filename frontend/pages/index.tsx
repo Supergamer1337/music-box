f2387fcad3a9ec1a5274452f241a029d1f509a5f
Supergamer1337 <felix.b.aronsson@gmail.com>
2022-05-16 19:17:29 +0200

    Styled login page for desktop

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -3,12 +3,14 @@ import AuthenticateWithDiscord from '../components/AuthenticateWithDiscord';
 
 const Home: NextPage = () => {
 	return (
-		<div className='h-[100vh] w-[100vw] bg-primaryBg pt-12'>
-			<div className='flex flex-col items-center'>
-				<h2 className='text-lg'>Welcome to</h2>
-				<h1 className='text-3xl'>GM's Music Box</h1>
+		<div className='h-[100vh] w-[100vw] sm:flex sm:items-center sm:justify-center'>
+			<div className='min-h-full sm:min-h-fit sm:max-h-min min-w-full sm:min-w-fit sm:max-w-min bg-primaryBg pt-12 sm:pt-8 sm:pb-24 sm:px-52 sm:rounded-3xl'>
+				<div className='flex flex-col items-center'>
+					<h2 className='text-lg'>Welcome to</h2>
+					<h1 className='text-3xl'>GM's Music Box</h1>
+				</div>
+				<AuthenticateWithDiscord className='m-auto mt-24' />
 			</div>
-			<AuthenticateWithDiscord className='m-auto mt-32' />
 		</div>
 	);
 };
