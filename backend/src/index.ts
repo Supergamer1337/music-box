f01459f130a31cf883231ddc776b842a7ac2cc67
Supergamer1337 <felix.b.aronsson@gmail.com>
2022-05-16 11:42:30 +0200

    Now authenticates bot

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log('Initial setup')
+import { Client, Intents } from 'discord.js'
+import { config } from 'dotenv'
+
+// Load environment variables
+config()
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+
+client.on('ready', () => {
+    if (!client.user) return console.error('Client user is undefined')
+    console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.login(process.env.BOT_TOKEN)
