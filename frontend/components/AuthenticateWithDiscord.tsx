f068b9b2968aa354059a0d0659a2d65df95713fc
Supergamer1337 <felix.b.aronsson@gmail.com>
2022-05-21 11:30:07 +0200

    Restructured authentication page

--- frontend/components/AuthenticateWithDiscord.tsx
+++ frontend/components/AuthenticateWithDiscord.tsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import DiscordSVG from './../svg/DiscordSVG';
+import React from 'react'
+import DiscordSVG from './../svg/DiscordSVG'
+import { useRouter } from 'next/router'
 
 type Props = {
-	className?: string;
-	onClick?: () => void;
-};
+    className?: string
+}
 
-const AuthenticateWithDiscord = ({
-	className = '',
-	onClick = () => {}
-}: Props) => {
-	return (
-		<div
-			className={`flex items-center w-fit bg-accent py-2 px-4 rounded shadow-md cursor-pointer hover:shadow-lg transition-shadow ${className}`}
-			onClick={() => onClick()}>
-			<DiscordSVG />
-			<p className='font-semibold text-lg h-fit w-fit ml-4 select-none'>
-				Authenticate
-			</p>
-		</div>
-	);
-};
+const AuthenticateWithDiscord = ({ className = '' }: Props) => {
+    const router = useRouter()
 
-export default AuthenticateWithDiscord;
+    return (
+        <div
+            className={`flex items-center w-fit bg-accent py-2 px-4 rounded shadow-md cursor-pointer hover:shadow-lg transition-shadow ${className}`}
+            onClick={() => {
+                // @ts-ignore
+                router.push(process.env.REDIRECT_ADDRESS)
+            }}
+        >
+            <DiscordSVG />
+            <p className="font-semibold text-lg h-fit w-fit ml-4 select-none">
+                Authenticate
+            </p>
+        </div>
+    )
+}
+
+export default AuthenticateWithDiscord
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,27 +1,7 @@
 import type { NextPage } from 'next'
-import { useRouter } from 'next/router'
-import AuthenticateWithDiscord from '../components/AuthenticateWithDiscord'
 
 const Home: NextPage = () => {
-    const router = useRouter()
-
-    return (
-        <div className="h-[100vh] w-[100vw] sm:flex sm:items-center sm:justify-center">
-            <div className="min-h-full sm:min-h-fit sm:max-h-min min-w-full sm:min-w-fit sm:max-w-min bg-primaryBg pt-12 sm:pt-8 sm:pb-24 sm:px-52 sm:rounded-3xl">
-                <div className="flex flex-col items-center">
-                    <h2 className="text-lg">Welcome to</h2>
-                    <h1 className="text-3xl">GM's Music Box</h1>
-                </div>
-                <AuthenticateWithDiscord
-                    className="m-auto mt-24"
-                    onClick={() => {
-                        // @ts-ignore
-                        router.push(process.env.REDIRECT_ADDRESS)
-                    }}
-                />
-            </div>
-        </div>
-    )
+    return <div>You are home.</div>
 }
 
 export default Home
