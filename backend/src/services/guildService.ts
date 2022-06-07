16ba2b722c0b8d6f4d237fad96d5ec1e3281a826
Supergamer1337 <felix.b.aronsson@gmail.com>
2022-06-07 10:29:22 +0200

    Added some comments to guildService

--- backend/src/services/guildService.ts
+++ backend/src/services/guildService.ts
@@ -1,12 +1,18 @@
 import { RESTGetAPICurrentUserGuildsResult } from '.pnpm/discord-api-types@0.30.0/node_modules/discord-api-types/v10'
 import { getRequest, handleRequestError } from './requestService.js'
 
-export const getDiscordUserGuilds = async (access_token: string) => {
+/**
+ * Gets the user's Discord guilds.
+ * @param accessToken The user's Discord access token.
+ * @returns The user's Discord guilds.
+ * @throws An error if the request failed.
+ */
+export const getDiscordUserGuilds = async (accessToken: string) => {
     try {
         return (await getRequest(
             'https://discord.com/api/v10/users/@me/guilds',
             {
-                Authorization: `Bearer ${access_token}`
+                Authorization: `Bearer ${accessToken}`
             }
         )) as RESTGetAPICurrentUserGuildsResult
     } catch (errorResponse: any) {
@@ -14,15 +20,19 @@ export const getDiscordUserGuilds = async (access_token: string) => {
     }
 }
 
-export const getDiscordGuild = async (
-    guildId: string,
-    access_token: string
-) => {
+/**
+ * Gets guild data from Discord for the given guild ID.
+ * @param guildId The guild ID.
+ * @param accessToken The user's Discord access token.
+ * @returns The guild data.
+ * @throws An error if the request failed.
+ */
+export const getDiscordGuild = async (guildId: string, accessToken: string) => {
     try {
         return await getRequest(
             'https://discord.com/api/v10/guilds/' + guildId,
             {
-                Authorization: `Bearer ${access_token}`
+                Authorization: `Bearer ${accessToken}`
             }
         )
     } catch (errorResponse: any) {
