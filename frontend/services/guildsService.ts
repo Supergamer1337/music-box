645065acf56c444554999be11b80d303c27afd22
Supergamer1337 <felix.b.aronsson@gmail.com>
2022-06-07 10:31:51 +0200

    Commented guildService in frontend

--- frontend/services/guildsService.ts
+++ frontend/services/guildsService.ts
@@ -6,18 +6,35 @@ import {
 import GuildListObject from '../types/GuildListObject'
 import { APIGuild } from 'discord-api-types/v10'
 
+/**
+ * Gets the user's Discord guilds, as the Next.js server.
+ * @param req The Next.js request object.
+ * @returns The user's Discord guilds.
+ * @throws An error if the request failed.
+ */
 export const serverGetGuilds = async (req: NextIncomingMessage) => {
     const guilds = await serverBackendGetRequest(req, '/api/v1/guilds')
 
-    return handleGuildRequest(guilds)
+    return handleGuildsRequest(guilds)
 }
 
+/**
+ * Gets the user's Discord guilds, as the client.
+ * @returns The user's Discord guilds.
+ * @throws An error if the request failed.
+ */
 export const clientGetGuilds = async () => {
     const guilds = await clientBackendGetRequest('/api/v1/guilds')
 
-    return handleGuildRequest(guilds)
+    return handleGuildsRequest(guilds)
 }
 
+/**
+ * Gets guild data from Discord for the given guild ID, as the Next.js server.
+ * @param req The Next.js request object.
+ * @returns The guild data.
+ * @throws An error if the request failed.
+ */
 export const serverGetGuildData = async (req: NextIncomingMessage) => {
     const guild = await serverBackendGetRequest(req, '/api/v1/guilds/:id')
 
@@ -28,10 +45,16 @@ export const serverGetGuildData = async (req: NextIncomingMessage) => {
     return (await guild.json()) as APIGuild
 }
 
-const handleGuildRequest = async (servers: Response) => {
-    if (!servers.ok) {
+/**
+ * Handles the guilds request response.
+ * @param guilds The guilds request response.
+ * @returns The guilds.
+ * @throws An error if the request failed.
+ */
+const handleGuildsRequest = async (guilds: Response) => {
+    if (!guilds.ok) {
         throw new Error('Failed to get servers')
     }
 
-    return (await servers.json()) as GuildListObject[]
+    return (await guilds.json()) as GuildListObject[]
 }
