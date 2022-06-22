caae99e632b6e452cdc838791be6566089fbdbc5
Supergamer1337 <felix.b.aronsson@gmail.com>
2022-06-22 10:06:44 +0200

    Refactored guildListItem

--- frontend/components/ErrorDiv.tsx
+++ frontend/components/ErrorDiv.tsx
@@ -8,7 +8,7 @@ interface Props {
 const ErrorDiv = ({ children, size }: Props) => {
     return (
         <p
-            className={`p-2 text-center bg-red-700 rounded-md  text-lg mx-4 ${
+            className={`p-2 text-center bg-red-700 rounded-md text-lg mx-4 ${
                 size === 'large' && 'mt-5 text-lg mx-4 p-2'
             } `}
         >
